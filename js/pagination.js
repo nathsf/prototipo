ffe09f32201e1720aec7afda6957c0998b2c0215
nathaly <nathalysalgado.f@gmail.com>
2020-07-17 09:58:52 -0500

    pagination

---
+++ js/pagination.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+    $('#example').DataTable( {
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
+        }
+    } );
+} );
